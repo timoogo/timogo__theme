7af27620095ad7a0a445eb5ac8ee30c085bc08ec
Timothée Gaultier <tiimogo@gmail.com>
2022-01-02 22:25:06 +0100

    sunday work

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,10 @@
-console.log('hello mix')
+console.log('hello mix');
+
+let section_list = document.querySelectorAll('section'); // returns NodeList
+let section_array = [...section_list]; // converts NodeList to Array
+section_array.forEach(section => {
+    section.addEventListener('click', bg())
+});
+function bg(){
+    this.dataset.is_filled = this.dataset.is_filled == "true" ? "false" : true;
+}
