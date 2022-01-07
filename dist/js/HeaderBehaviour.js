e37d19f92d121d2e2df82bfa6ee0e8870fabbaad
Timothée Gaultier <tiimogo@gmail.com>
2022-01-07 19:05:04 +0100

    b4 header mobile

--- dist/js/HeaderBehaviour.js
+++ dist/js/HeaderBehaviour.js
@@ -1,6 +1,6 @@
     const header = document.querySelector("header");
     const sectionOne = document.querySelector(".hero");
-    
+    const burger = document.querySelector('#navbar-toggle')
     const sectionOneOptions = {
       rootMargin: "-80px 0px 0px 0px"
     };
@@ -12,8 +12,12 @@
       entries.forEach(entry => {
         if (!entry.isIntersecting) {
           header.classList.add("scrolled");
+          burger.classList.add("scrolled");
+          console.log(burger)
         } else {
           header.classList.remove("scrolled");
+           burger.classList.remove("scrolled");
+          console.log("burger rem")
         }
       });
     },
