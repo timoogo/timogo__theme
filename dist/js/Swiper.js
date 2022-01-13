72a40a1c435401d05d504e25df3b7d63989680c7
Timothée Gaultier <tiimogo@gmail.com>
2022-01-13 21:57:32 +0100

    slider mobile corrected

--- dist/js/Swiper.js
+++ dist/js/Swiper.js
@@ -1,37 +1,77 @@
 //console.clear();
 
 window.onload = () => {
-  const swiperConfig = {
-    cssMode:true,
-    mousewheel: false,
-    mousewheelControl: false,
-    loop: false,
-    parallax: true,
-    slidesPerView: "auto",
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    breakpoints: {
+  // const swiperConfig = {
+  //   cssMode:true,
+  //   mousewheel: false,
+  //   mousewheelControl: false,
+  //   loop: false,
+  //   parallax: true,
+  //   slidesPerView: "auto",
+  //   navigation: {
+  //     nextEl: ".swiper-button-next",
+  //     prevEl: ".swiper-button-prev",
+  //   },
+  //   pagination: {
+  //     el: ".swiper-pagination",
+  //     clickable: true,
+  //   },
+  //   breakpoints: {
    
-      740: {
-        slidesPerView: 1,
-        spaceBetween: 200,
-        loop:true
-      },
-      1024: {
-        slidesPerView: 3,
-        spaceBetween: 50,
-      },
-    }
-  }
-  const divswipe = new Swiper('.mySwiper', swiperConfig);
+  //     740: {
+  //       slidesPerView: 1,
+  //       spaceBetween: 200,
+  //       loop:true
+  //     },
+  //     1024: {
+  //       slidesPerView: 3,
+  //       spaceBetween: 50,
+  //     },
+  //   }
+  // }
+  // const divswipe = new Swiper('.mySwiper', swiperConfig);
 
 };
+
+const observer = new ResizeObserver(entries =>{
+
+  const myObserver = new ResizeObserver(entries => {
+    for (let entry of entries) {
+      const swiperConfig = {
+        cssMode:true,
+        mousewheel: false,
+        mousewheelControl: false,
+        loop: false,
+        parallax: true,
+        slidesPerView: "auto",
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+        breakpoints: {
+       
+          740: {
+            slidesPerView: 1,
+            spaceBetween: 200,
+            loop:true
+          },
+          1024: {
+            slidesPerView: 3,
+            spaceBetween: 50,
+          },
+        }
+      }
+      const divswipe = new Swiper('.mySwiper', swiperConfig);
+        }
+})
+
+myObserver.observe(divswipe);
+
+
 let resizeTimeout;
 window.addEventListener('resize', function(event) {
   clearTimeout(resizeTimeout);
